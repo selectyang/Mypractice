633b609a075e79d67b83aae59c59c12c559eae9c
yanjiawen <yangjiawen_01@163.com>
2017-08-20 17:13:52 +0800

    creat

---
+++ gulp_5/js/Carousel.js
@@ -0,0 +1,118 @@
+function Carousel($container) {
+	this.container = $container;
+	this.init();
+	this.resize();
+	this.bind();
+	this.autoPlay(3000);
+}
+
+Carousel.prototype.init = function () {
+	var $buttonPre = this.$buttonPre = this.container.find('.pre'),
+		$buttonNext = this.$buttonNext = this.container.find('.next'),
+		$imgCarousel = this.$imgCarousel = this.container.find('.carousel'),
+		$imgContent = this.$imgContent = this.container.find('.img-content'),
+		$imgs = this.$imgs = this.container.find('.img-content img'),
+		$bullets = this.$bullets = this.container.find('.bullet >li');
+
+	this.pageIndex = 0;
+	this.islock = true;
+
+	this.imgLen = $imgs.length;
+	//设置图片初始宽度= 窗口宽度
+	this.$imgs.width(window.innerWidth);
+
+	$imgContent.append($imgs.first().clone());
+	$imgContent.prepend($imgs.last().clone());
+	//设置轮播容器初始宽度
+	$imgContent.css('width', $('.img-content img').length * (window.innerWidth));
+	$imgContent.css('left', -window.innerWidth);
+};
+
+Carousel.prototype.bind = function () {
+	var _this = this;
+	this.$buttonPre.on('click', function (e) {
+		e.preventDefault();
+		if (_this.islock)
+			_this.playPre(1);
+	});
+	this.$buttonNext.on('click', function (e) {
+		e.preventDefault();
+		if (_this.islock)
+			_this.playNext(1);
+	});
+	this.$bullets.on('click', function () {
+		var index = $(this).index();
+		// console.log('index',index);
+		if (index > _this.pageIndex) {
+			_this.playNext(index - _this.pageIndex);
+		} else if (index < _this.pageIndex) {
+			_this.playPre(_this.pageIndex - index);
+		}
+	});
+};
+//窗口改变是从新设置轮播图大小	
+Carousel.prototype.resize = function () {
+	var _this = this;
+	$(window).resize(function () {
+		//console.log('resize_start')
+		_this.container.find('.img-content img').width(window.innerWidth);//设置改变窗口后的图片宽度
+		_this.$imgContent.css('width', $('.img-content img').length * window.innerWidth);//设置改变窗口后轮播容器的宽度
+		_this.$imgContent.css('left', -window.innerWidth);	//设置新的轮播距离
+		//console.log(_this.$imgContent,$('.img-content img').length* window.innerWidth)
+		//console.log('resize_end')
+	});
+};
+
+
+Carousel.prototype.autoPlay = function (time) {
+	var _this = this,
+	timeClock = setInterval(function () {
+		_this.playNext(1);
+	}, time);
+};
+
+Carousel.prototype.playNext = function (len) {
+	var _this = this;
+	this.islock = false;
+	this.$imgContent.animate({
+		left: '-=' + window.innerWidth * len
+	}, function () {
+		_this.pageIndex += len;
+		if (_this.pageIndex === _this.imgLen) {
+			_this.pageIndex = 0;
+			_this.$imgContent.css('left', -window.innerWidth);
+		}
+		_this.islock = true;
+		_this.setbullet();
+	});
+};
+
+Carousel.prototype.playPre = function (len) {
+	var _this = this;
+	_this.islock = false;
+	this.$imgContent.animate({
+		left: '+=' + window.innerWidth * len
+	}, function () {
+		_this.pageIndex -= len;
+		if (_this.pageIndex < 0) {
+			_this.pageIndex = _this.imgLen - 1;
+			_this.$imgContent.css('left', -_this.imgLen * window.innerWidth);
+		}
+		_this.islock = true;
+		_this.setbullet();
+	});
+};
+
+Carousel.prototype.setbullet = function () {
+	this.$bullets.removeClass('active')
+		.eq(this.pageIndex)
+		.addClass('active');
+};
+
+function carousel ($ct) {
+    $ct.each(function (index, node) {
+        new Carousel($(node));
+    });
+}
+
+carousel($('.container'));
---
+++ gulp_5/js/Waterfall.js
@@ -0,0 +1,101 @@
+//1. 获取数据
+//http://platform.sina.com.cn/slide/album_tech?jsoncallback=func&app_key=1271687855&num=3&page=4
+//2. 把数据变为 Dom，通过瀑布流的方式放到页面上
+//3. 当滚动到底部的时候， --》 1
+
+//1. 获取数据
+//http://platform.sina.com.cn/slide/album_tech?jsoncallback=func&app_key=1271687855&num=3&page=4
+//2. 把数据变为 Dom，通过瀑布流的方式放到页面上
+//3. 当滚动到底部的时候， --》 1
+
+function Waterfall() {
+	this.init();
+	this.bind();
+}
+
+Waterfall.prototype.init = function () {
+	var perPageCount = 10,
+		pageIndex = 1;
+	this.getNews();
+};
+
+Waterfall.prototype.insetNode = function (ret) {
+	var _this = this;
+	$.each(ret,function(index,news){
+		var nodeTemps = '';
+		nodeTemps +='<div class="item">';
+		nodeTemps += '<a class="link" href="'+ news.url +'">';
+		nodeTemps += '<img src="' + news.img_url + '" alt=""></a>';
+		nodeTemps += '<h4 class="header" >' + news.name + '</h4>';
+		nodeTemps += '<p class="text" >' + news.short_intro+ '</p>';
+		nodeTemps += '</div>';
+		$(nodeTemps).find('img').load(function(){
+				$('.waterfall').append($(nodeTemps));	
+				_this.render();
+		});
+	});
+};
+
+
+Waterfall.prototype.bind = function () {
+	var _this = this;
+	$('.load').on('click', function () {
+		_this.getNews();
+	});
+};
+
+Waterfall.prototype.getNews = function () {
+	var _this = this;
+	$.ajax({
+		url:"http://platform.sina.com.cn/slide/album_tech",
+		dataType: 'jsonp',
+		jsonp:"jsoncallback",
+		data: {
+			app_key: '1271687855',
+			num: _this.perPageCount,
+			page: _this.pageIndex
+		}
+	}).done(function (ret) {
+		console.log(ret.status.code );
+		if (ret.status.code === '0') {
+			_this.insetNode(ret.data);  //如果数据没问题，那么生成节点插入页面同时摆放好位置
+			_this.pageIndex++;
+		} else {
+			console.log("get error data");
+		}
+	});
+};
+
+Waterfall.prototype.render = function () {
+	// console.log(xxx);
+	var nodeWidth = $('.item').outerWidth(true),
+		nodeNum = parseInt($('.waterfall').width() / nodeWidth),
+		nodeSumHeight = [];
+
+	for (var i = 0; i < nodeNum; i++) {
+		nodeSumHeight.push(0);
+	}
+
+	$('.item').each(function () {
+		var $cur = $(this),
+			index = 0,
+			minSumHeight = nodeSumHeight[0];
+		for (var i = 0; i < nodeSumHeight.length; i++) {
+			if (nodeSumHeight[i] < minSumHeight) {//找出最小高度
+				index = i;
+				minSumHeight = nodeSumHeight[i];
+				//console.log('index:'+(cont++),index)
+			}
+		}
+		$cur.css({
+			left: nodeWidth * index,
+			top: minSumHeight
+		});
+		nodeSumHeight[index] += $cur.outerHeight(true);
+		// console.log(nodeSumHeight)
+		$('.waterfall').height(Math.max.apply(null, nodeSumHeight));
+	});
+};
+
+
+new Waterfall();
